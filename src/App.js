08dc8c10f2365eb030eedeed5f73d2ebdd072f11
sahil5201 <62256341+sahil5201@users.noreply.github.com>
2020-03-18 23:21:41 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -14,22 +14,10 @@ function App() {
       <th>Username</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody>   
     <tr>
       <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
+      <td>Sahil5201</td>
       <td>@twitter</td>
     </tr>
   </tbody>
